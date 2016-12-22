57a1ba1a06b6db8e94f84d5e9cf60e3052a14ffc
prinston23 <prinstonabi@gmail.com>
2016-12-22 20:47:46 +0530

    first commit

---
+++ src/ConversionDemo.java
@@ -0,0 +1,11 @@
+
+public class ConversionDemo {
+
+	public static void main(String[] args) {
+		double celsious,farenheit;
+		farenheit=100;
+		celsious=(farenheit-30)*5/9;
+		System.out.println(" Celsious valuse: "+celsious );
+	}
+
+}
